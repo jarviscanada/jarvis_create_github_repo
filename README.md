2a2cf8fb2dbccc02e6a80c72bd5fe7114f5c0d57
Edward Wang <edward@jrvs.ca>
2020-05-30 08:12:14 -0400

    add scripts and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # jarvis_create_github_repo
+Create Jarvis Github repos in batch.
+# Usage
+
+Input csv file format: `FirstnameLastname,github_username`
+
+Script usage:
+
+```bash
+./create_github_repo.sh github_token input_csv
+
+e.g.
+./create_github_repo.sh 2sd8sdf8d8f8f github.csv
+```
---
+++ create_github_repo.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+github_token=$1
+csv_file=$2
+
+REPO_PREFIX="jarvis_data_eng_"
+USER_NAME="jarviscanada"
+
+repos_json=$(curl -s --location --request GET 'https://api.github.com/user/repos' --header "Authorization: token ${github_token}")
+
+generate_id() {
+    repo_name=$1
+    count=$(echo "$repos_json" | grep '"name": "' | grep -wi ${repo_name} | wc -l)
+    if [ $count -eq 0 ]; then
+        echo ""
+    else
+        echo $((count + 1))
+    fi
+}
+
+check_http_status() {
+    http_response=$1
+    expected_status=$2
+    action=$3
+    status=$(echo "$1" | head -1 | awk '{print $2}' | xargs)
+    if [ "$status" = "$2" ]; then
+        echo "----Sccuss: $action"
+    else
+        echo >&2 "----Failed: $action"
+    fi
+}
+
+create_github_repo() {
+    username=$1
+    repo_name=${REPO_PREFIX}${username}
+    id=$(generate_id ${repo_name})
+    repo_name=${repo_name}${id}
+    collaborator=$2
+
+    echo ">>>>processing $repo_name"
+
+    #create repo
+    sleep 1
+    http_response=$(curl -i -s --location --request POST 'https://api.github.com/user/repos' \
+        --header "Authorization: token ${github_token}" \
+        --header 'Content-Type: application/json' \
+        --data-raw '{
+	"name": "'${repo_name}'"
+}')
+    check_http_status "${http_response}" 201 "create repo"
+
+    #add collaborator
+    sleep 1
+    http_response=$(curl -i -s --location --request PUT "https://api.github.com/repos/${USER_NAME}/${repo_name}/collaborators/${collaborator}" \
+        --header "Authorization: token ${github_token}")
+    check_http_status "${http_response}" 201 "add collaborator"
+
+    #stop watching
+    sleep 1
+    http_response=$(curl -i -s --location --request DELETE 'https://api.github.com/repos/'${USER_NAME}'/'${repo_name}'/subscription' \
+        --header 'Authorization: token '${github_token}'')
+    check_http_status "${http_response}" 204 "stop watching"
+
+    echo ">>>>Finish $repo_name"
+    echo ""
+}
+
+parse_row() {
+    row=$1
+    echo $row | awk -F',' '{print $1 " " $2}'
+}
+
+for row in $(cat $csv_file); do
+    create_github_repo $(parse_row ${row})
+done
